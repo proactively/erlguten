940b060cd0aa9840928a8da21d03d9339b0d1486
Sean Hinde <sean@engaged-systems.com>
2018-11-08 18:32:01 +0100

    Don't register the pdf process as a named process.
    My application requires concurrency here

--- src/eg_pdf.erl
+++ src/eg_pdf.erl
@@ -592,7 +592,7 @@ inBuiltFonts() ->
 %% Description: Starts the server
 %%--------------------------------------------------------------------
 start_link(Init) ->
-    gen_server:start_link({local,pdf}, ?MODULE, Init, []).
+    gen_server:start_link(?MODULE, Init, []).
 
 %%====================================================================
 %% gen_server callbacks
